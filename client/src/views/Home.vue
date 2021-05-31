28f9a3104c60f593687f7953c64c70bfbe4748a5
Kacper Kwietniewski <48916089+kkwietniewski@users.noreply.github.com>
2021-05-31 23:26:33 +0200

    Prepared laravel starter backend & vue

---
+++ client/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home">
+        {{ info }}
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+
+export default {
+    name: "Home",
+    components: {},
+    data() {
+        return {
+            info: null,
+        };
+    },
+    mounted() {
+        axios
+            .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+            .then((response) => (this.info = response));
+    },
+};
+</script>
